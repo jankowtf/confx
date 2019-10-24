7e3d4a286e4b9265b8c1affbe49db261a7716f47
Janko Thyson <janko.thyson@rappster.de>
2019-10-25 00:03:16 +0200

    Based call to load() in .onAttach() on env variable CONFX_AUTO_LOAD

--- R/startup.R
+++ R/startup.R
@@ -3,24 +3,15 @@
   Sys.setenv(TZ = "UTC")
   Sys.setenv(language = "en")
 
-  # Load configs -----
-  print(here::here())
-  # confx::load()
-
-  # For plumber testing -----
-  # .__STATE__ <<- new.env(parent = emptyenv()) #create .state when package is first loaded
-
   invisible(TRUE)
 }
 
 .onAttach <- function(libname, pkgname) {
-  # options(digits.secs = 3)
-  # Sys.setenv(TZ = "UTC")
-  # Sys.setenv(language = "en")
-
   # Load configs -----
-  print(here::here())
-  confx::load()
+  env_auto_load <- as.logical(Sys.getenv("CONFX_AUTO_LOAD", FALSE))
+  if (env_auto_load) {
+    confx::load()
+  }
 
   # For plumber testing -----
   # .__STATE__ <<- new.env(parent = emptyenv()) #create .state when package is first loaded
